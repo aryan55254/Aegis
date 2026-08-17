2f96e403f0d07e664e9465d5a9ae11c7eb738c29
aryanmishra55254 <aryanmishra55254@gmail.com>
2026-08-17 21:16:58 +0530

    free_all func added

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 static unsigned char static_memory[1024 * 1024];
 static unsigned char *arena_buffer = static_memory;
 static size_t arena_buffer_length = sizeof(static_memory);
-static size_t arena_offset;
+static size_t current_offset;
+static size_t prev_offset;
 
 // memory alignment must be by power of two (1 2 4 8 16...) so in this function we check if they are divisible and if not we align them for faster access
 static uintptr_t align_forward(uintptr_t ptr, size_t align)
@@ -18,14 +19,15 @@ static uintptr_t align_forward(uintptr_t ptr, size_t align)
 
 void *arena_alloc(size_t size, size_t align = alignof(std::max_align_t))
 {
-    uintptr_t current_ptr = (uintptr_t)&arena_buffer[arena_offset]; // we grab the pointer to the next available pyte in the arena and convert it to an integer to perform arithmentic on it
-    uintptr_t aligned_ptr = align_forward(current_ptr, align);      // aligns the pointer to nearest multiple of of align
+    uintptr_t current_ptr = (uintptr_t)&arena_buffer[current_offset]; // we grab the pointer to the next available byte in the arena and convert it to an integer to perform arithmentic on it
+    uintptr_t aligned_ptr = align_forward(current_ptr, align);        // aligns the pointer to nearest multiple of of align
 
     size_t padding = aligned_ptr - current_ptr; // calculates sacrifical bytes to align
 
-    if (arena_offset + padding + size <= arena_buffer_length)
+    if (current_offset + padding + size <= arena_buffer_length)
     {
-        arena_offset += padding + size;
+        prev_offset = current_offset;
+        current_offset += padding + size;
         void *ptr = (void *)aligned_ptr;
         memset(ptr, 0, size);
         return ptr;
@@ -33,3 +35,10 @@ void *arena_alloc(size_t size, size_t align = alignof(std::max_align_t))
 
     return nullptr;
 }
+
+void free_all()
+{
+    prev_offset = current_offset;
+    current_offset = 0;
+}
+
