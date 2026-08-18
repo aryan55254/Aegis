4d408f7433816a1a5212ead42999e165d0ec3c8e
aryanmishra55254 <aryanmishra55254@gmail.com>
2026-08-18 23:53:39 +0530

    secure boundary in constructor  on max size that can be allocated for now  also update hpp file for new non zeroing based allocation

--- include/arena.hpp
+++ include/arena.hpp
@@ -3,6 +3,7 @@
 #include <cstddef>
 #include <cstring>
 #include <cstdint>
+#include <fmt/core.h>
 
 class Arena
 {
@@ -12,7 +13,16 @@ private:
     size_t current_offset;
 
 public:
-    explicit Arena(size_t size) : arena_buffer(new unsigned char[size]), arena_buffer_length(size), current_offset(0) {}
+    explicit Arena(size_t size)
+    {
+        if (size > 1024 * 1024)
+        {
+            fmt::print("Allocations above 1MB not allowed as of now");
+        }
+        arena_buffer = new unsigned char[size];
+        arena_buffer_length = size;
+        current_offset = 0;
+    }
 
     ~Arena()
     {
@@ -25,6 +35,8 @@ public:
     static uintptr_t align_forward(uintptr_t ptr, size_t align);
 
     void *arena_alloc(size_t size, size_t align = alignof(std::max_align_t));
+
+    void *arena_alloc_zeroed(size_t size, size_t align = alignof(std::max_align_t));
 
     void *arena_resize(size_t old_size, void *old_memory, size_t new_size, size_t new_align);
 };
