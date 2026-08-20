d9f6503da0ad3c6b030d4893e4aebaad409a0777
aryanmishra55254 <aryanmishra55254@gmail.com>
2026-08-20 23:06:26 +0530

    enhance arena memory management with allocation checks and resizing logic

--- src/arena.cpp
+++ src/arena.cpp
@@ -16,8 +16,10 @@ void *Arena::arena_alloc(size_t size, size_t align = alignof(std::max_align_t))
 
     size_t padding = aligned_ptr - current_ptr; // calculates sacrifical bytes to align
 
+    // check if there is space vailable
     if (current_offset + padding + size <= arena_buffer_length)
     {
+        // increase the offset and return the pointer to the memory
         current_offset += padding + size;
         void *ptr = (void *)aligned_ptr;
         return ptr;
@@ -26,12 +28,13 @@ void *Arena::arena_alloc(size_t size, size_t align = alignof(std::max_align_t))
     return nullptr;
 }
 
+// in this we do the exact same shit as arena alloc but also makes sure all the memory has standard 0 stored in it by default via memset which is not done in fucntion above it still has random shit stored there but it doesn't matter coz when programmer use this memory the programmer will rewrite it anyways
 void *Arena::arena_alloc_zeroed(size_t size, size_t align = alignof(std::max_align_t))
 {
-    uintptr_t current_ptr = (uintptr_t)&arena_buffer[current_offset]; // we grab the pointer to the next available byte in the arena and convert it to an integer to perform arithmentic on it
-    uintptr_t aligned_ptr = align_forward(current_ptr, align);        // aligns the pointer to nearest multiple of of align
+    uintptr_t current_ptr = (uintptr_t)&arena_buffer[current_offset];
+    uintptr_t aligned_ptr = align_forward(current_ptr, align);
 
-    size_t padding = aligned_ptr - current_ptr; // calculates sacrifical bytes to align
+    size_t padding = aligned_ptr - current_ptr;
 
     if (current_offset + padding + size <= arena_buffer_length)
     {
@@ -44,47 +47,66 @@ void *Arena::arena_alloc_zeroed(size_t size, size_t align = alignof(std::max_ali
     return nullptr;
 }
 
+// frees all memory
 void Arena::free_all()
 {
     current_offset = 0;
 }
 
-// can only rezise the last allocaton and only increase it not discrease it
-void *Arena::arena_resize(size_t old_size, void *old_memory, size_t new_size, size_t new_align)
+// can only grow or shrink the last allocation nothing else can be done
+void *Arena::arena_resize(size_t old_size, void *old_memory, size_t new_size)
 {
-
-    if (old_memory == NULL)
+    // null ptrs are not valid here
+    if (old_memory == nullptr)
     {
-        return arena_alloc(new_size, new_align);
-    }
-
-    if (new_size <= old_size)
-    {
-        return old_memory;
+        return nullptr;
     }
 
     uintptr_t current_ptr = (uintptr_t)old_memory;
 
-    if (current_ptr + old_size == (uintptr_t)&arena_buffer[current_offset])
+    // check if this is the last allocation made in the arena
+    if (current_ptr + old_size != (uintptr_t)&arena_buffer[current_offset])
     {
-        size_t size_diff = new_size - old_size;
-        if (current_offset + size_diff <= arena_buffer_length)
-        {
-            current_offset += size_diff;
-            void *ptr = (void *)current_ptr;
-            memset(ptr, 0, size_diff);
-            return ptr;
-        }
         return nullptr;
     }
+
+    // if size is the same then nothing needs to change
+    if (new_size == old_size)
+    {
+        return old_memory;
+    }
+
+    // if new size is smaller then give the unused space back to the arena
+    if (new_size < old_size)
+    {
+        size_t size_diff = old_size - new_size;
+        current_offset -= size_diff;
+        return old_memory;
+    }
+
+    // if new size is bigger then try to extend the allocation forward
+    size_t size_diff = new_size - old_size;
+
+    if (current_offset + size_diff <= arena_buffer_length)
+    {
+        current_offset += size_diff;
+
+        // zero only the newly added memory
+        memset((unsigned char *)current_ptr + old_size, 0, size_diff);
+
+        return old_memory;
+    }
+
     return nullptr;
 }
 
+// returns arena buffer length
 size_t Arena::capacity() const
 {
     return arena_buffer_length;
 }
 
+// returns current arena offset
 size_t Arena::offset() const
 {
     return current_offset;
